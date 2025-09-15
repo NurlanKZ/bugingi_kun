a1b07ee960a16d246d897c267685fc70d5e8b729
Nurlan Kudaibergen <nurlan96@gmail.com>
2025-09-15 16:47:10 +0500

    v0.1

---
+++ pytgbot.py
@@ -0,0 +1,67 @@
+import os
+import time
+import asyncio
+import schedule
+import requests
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
+from telegram.ext import ApplicationBuilder
+
+
+
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+
+CHANNEL_ID = os.environ.get('CHANNEL_ID')
+
+# Create the Application and pass it your bot's token.
+async def send_daily_message(app):
+    url = "https://sputnik.kz/news/"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
+
+    soup = BeautifulSoup(requests.get(url, headers=headers).text, 'html.parser')
+    links = soup.find_all('a', href=True)
+
+    filtered_hrefs = [link['href'] for link in links if "bugin-alemde-zhane-qazaqstanda-qanday-mereke-" in link['href']]
+
+    soup = BeautifulSoup(requests.get(filtered_hrefs[0], headers=headers).text, 'html.parser')
+    divs = soup.find_all('div', class_='article__block')
+
+    output = ""
+    for div in divs[2:]:
+        text = div.get_text(separator=' ', strip=True)
+        output += text + "\n\n"
+
+    output = output.rpartition(";")[0]
+
+    await app.bot.send_message(chat_id=CHANNEL_ID, text=output)
+
+def job(app):
+    # Create an asyncio task for sending the message without blocking
+    asyncio.create_task(send_daily_message(app))
+
+async def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+    # Start the application (polling won't be strictly necessary if only scheduling)
+    await app.initialize()
+    await app.start()
+    
+    # Schedule job daily at 09:00
+    schedule.timezone = 'Asia/Almaty'  # Set your timezone if needed
+    schedule.every().day.at("00:02").do(job, app)
+
+    while True:
+        schedule.run_pending()
+        await asyncio.sleep(1)  # Use asyncio sleep in async function
+
+    await app.stop()
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
+
+
+    
+
